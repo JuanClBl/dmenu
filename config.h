28fce581f1f3d645f1312ef318954cf87f1019ca
JuanClBl <juan.clement.bellido@gmail.com>
2021-12-10 20:31:10 +0100

    Added border patch

--- config.h
+++ config.h
@@ -3,7 +3,6 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xe0;
-static unsigned int border_width = 0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"NotoSansMono Nerd Font:size=10:style=Bold:antialias=true:autohint=true",
@@ -31,3 +30,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 5;
